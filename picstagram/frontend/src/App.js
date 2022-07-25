2454a660e9e0dd37d564ae9fcd2cf5dbca2b993a
Anderson Santana <104103694+Andersons1218@users.noreply.github.com>
2022-07-25 12:40:12 -0400

    greate routes and pages for user auth

--- picstagram/frontend/src/App.js
+++ picstagram/frontend/src/App.js
@@ -2,6 +2,14 @@
 import './App.css';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/homePage";
+import Login from "./views/loginPage";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/ProtectedPage";
 
 export default function App() {
   const [posts, setPosts] = useState([]);
@@ -22,11 +30,27 @@ export default function App() {
     getPost()
   }, [])
   return (
+    <>
+     <Router>
+      <div className="flex flex-col min-h-screen overflow-hidden">
+        <AuthProvider>
+          <Navbar />
+          <Switch>
+            <PrivateRoute component={ProtectedPage} path="/protected" exact />
+            <Route component={Login} path="/login" />
+            <Route component={Register} path="/register" />
+            <Route component={Home} path="/" />
+          </Switch>
+        </AuthProvider>
+        <Footer />
+      </div>
+    </Router>
     <div>
       {posts.map((post) => (
         <div>
           {post.comments}</div>))}
     </div>
+    </>
   );
 
 }
